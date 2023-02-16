d3ef7b94e7503ac185fccca8f18ae3ad34d02a4f
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2023-02-16 14:04:19 -0500

    Initial Commit

---
+++ HorizontalBarChart/HorizontalBarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalBarChartApp.swift
+//  HorizontalBarChart
+//
+//  Created by Jo Lingenfelter on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalBarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
